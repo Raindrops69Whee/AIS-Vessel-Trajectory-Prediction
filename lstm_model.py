962aaf55e2657d111db6648c59d384f6267b77d9
d3term1na <80019102+d3term1na@users.noreply.github.com>
2023-01-01 17:14:48 +0800

    lstm_seq2seq_with_att done

--- lstm_model.py
+++ lstm_model.py
@@ -11,7 +11,7 @@ from torch.nn import functional as F
 import config_trAISformer as config_trAISformer
 
 logger = logging.getLogger(__name__)
-device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device=torch.device("cuda")
 
 config = config_trAISformer.Config()
 
@@ -29,36 +29,27 @@ class Attention(nn.Module):
         
     
     def forward(self, hidden_dec, last_layer_enc):
-        ''' 
-            PARAMS:           
-                hidden_dec:     [b, n_layers, n_hidden_dec]    (1st hidden_dec = encoder's last_h's last layer)                 
-                last_layer_enc: [b, seq_len, n_hidden_enc * 2] 
-            
-            RETURN:
-                att_weights:    [b, src_seq_len] 
-        '''
         batch_size = last_layer_enc.size(0)
         src_seq_len = last_layer_enc.size(1)
 
-        hidden_dec = hidden_dec[-1, :, :].unsqueeze(1).repeat(1, src_seq_len, 1)         #[b, src_seq_len, n_hidden_dec]
+        hidden_dec = hidden_dec[-1, :, :].unsqueeze(1).repeat(1, src_seq_len, 1)
 
-        tanh_W_s_h = torch.tanh(self.W(torch.cat((hidden_dec, last_layer_enc), dim=2)))  #[b, src_seq_len, n_hidden_dec]
+        tanh_W_s_h = torch.tanh(self.W(torch.cat((hidden_dec, last_layer_enc), dim=2)))
 
-        tanh_W_s_h = tanh_W_s_h.permute(0, 2, 1)       #[b, n_hidde_dec, seq_len]
+        tanh_W_s_h = tanh_W_s_h.permute(0, 2, 1)
         
-        V = self.V.repeat(batch_size, 1).unsqueeze(1)  #[b, 1, n_hidden_dec]
-        e = torch.bmm(V, tanh_W_s_h).squeeze(1)        #[b, seq_len]
+        V = self.V.repeat(batch_size, 1).unsqueeze(1)
+        e = torch.bmm(V, tanh_W_s_h).squeeze(1)
         
-        att_weights = F.softmax(e, dim=1)              #[b, src_seq_len]
+        att_weights = F.softmax(e, dim=1)
         
         return att_weights
     
 
     
 class EncoderRNN(nn.Module):
-    def __init__(self, input_size, hidden_size):
+    def __init__(self, hidden_size):
         super(EncoderRNN, self).__init__()
-        self.input_size = input_size
         self.hidden_size = hidden_size
         self.lstm = nn.LSTM(hidden_size, hidden_size, batch_first = True, bidirectional = True)
 
@@ -66,51 +57,14 @@ class EncoderRNN(nn.Module):
         output, hidden = self.lstm(input, hidden)
         return output, hidden
 
-    def initHidden(self):
-        return (torch.zeros(2, self.input_size, self.hidden_size, device=device), torch.zeros(2, self.input_size, self.hidden_size, device=device))
-
-# class DecoderRNN(nn.Module):
-#     def __init__(self, hidden_size, output_size, dropout_p=0.1):
-#         super(DecoderRNN, self).__init__()
-#         self.hidden_size = hidden_size
-#         self.dropout_p = dropout_p
-#         self.embedding = nn.Embedding(output_size, hidden_size)
-#         self.dropout = nn.Dropout(self.dropout_p)
-#         self.lstm = nn.LSTM(hidden_size * 2, hidden_size, batch_first = True)
-#         self.out = nn.Linear(hidden_size * 2, output_size)
-#         self.softmax = nn.LogSoftmax(dim=1)
-
-#     def forward(self, input, hidden, encoder_outputs, attn_weights):
-#         embedded = self.embedding(input.type(torch.LongTensor))
-#         embedded = self.dropout(embedded)
-#         print(embedded.shape)
-#         exit()
-#         attn_applied = torch.bmm(attn_weights.unsqueeze(1),
-#                                  encoder_outputs)
-
-#         output = torch.cat((embedded, attn_applied), 2)
-#         output = self.attn_combine(output).unsqueeze(0)
-
-#         output = F.relu(output)
-#         output, hidden = self.lstm(output, hidden)
-
-#         output = F.log_softmax(self.out(output[0]), dim=1)
-#         return output, hidden, attn_weights
-#         # output = F.relu(output)
-#         # output, hidden = self.lstm(output, hidden)
-#         # output = self.softmax(self.out(output[0]))
-#         # return output, hidden
-
-#     def initHidden(self):
-#         return (torch.zeros(1, self.input_size, self.hidden_size, device=device), torch.zeros(1, self.input_size, self.hidden_size, device=device))
+    def initHidden(self, batch_size):
+        return (torch.zeros(2, batch_size, self.hidden_size, device=device), torch.zeros(2, batch_size, self.hidden_size, device=device))
 
 class Decoder(nn.Module):
-    
     def __init__(self, n_embed, n_hidden_enc, n_hidden_dec, n_output = 1, n_layers=2, dropout=0.1):
         super().__init__()
 
         self.n_output = n_output
-        self.embedding = nn.Embedding(n_output, n_embed)
         
         self.lstm = nn.LSTM(n_embed + n_hidden_enc*2, n_hidden_dec, n_layers,
                           bidirectional=False, batch_first=True,
@@ -124,127 +78,25 @@ class Decoder(nn.Module):
         
     
     def forward(self, target, hidden_dec, last_layer_enc):
-        ''' 
-            target:         [b, n_embed] 
-            hidden_dec:     [b, n_layers, n_hidden_dec]      (1st hidden_dec = encoder's last_h's last layer)
-            last_layer_enc: [b, seq_len, n_hidden_enc * 2]   (* 2 since bi-directional)  
-            --> not to be confused with encoder's last hidden state. last_layer_enc is ALL hidden states (of timesteps 0,1,...,t-1) of the last LAYER.
-        '''
+        embedded_trg = target.unsqueeze(1)
+ 
+        att_weights = self.attention(hidden_dec[0], last_layer_enc)
+        att_weights = att_weights.unsqueeze(1)
+
+        weighted_sum = torch.bmm(att_weights, last_layer_enc)
         
-        ######################## 1. TARGET EMBEDDINGS ######################### 
-        embedded_trg = target.unsqueeze(1) #[b, 1] : since n_output = 1
-
-
-        ################## 2. CALCULATE ATTENTION WEIGHTS #####################      
-        att_weights = self.attention(hidden_dec[0], last_layer_enc)  #[b, input_seq_len]
-        att_weights = att_weights.unsqueeze(1)   #[b, 1, input_seq_len]
-
-
-        ###################### 3. CALCULATE WEIGHTED SUM ######################
-        weighted_sum = torch.bmm(att_weights, last_layer_enc) #[b, 1, n_hidden_enc*2]
-        
-        
-        ############################# 4. GRU LAYER ############################
-        lstm_input = torch.cat((embedded_trg, weighted_sum), dim=2) #[b, 1, n_embed + n_hidden_enc*2]
+        lstm_input = torch.cat((embedded_trg, weighted_sum), dim=2)
         last_layer_dec, last_h_dec = self.lstm(lstm_input, hidden_dec)
-        # last_layer_dec: [b, trg_seq_len, n_hidden_dec]
-
-        # last_h_dec = (last_h_dec[0].permute(1, 0, 2), last_h_dec[1].permute(1, 0, 2))  #[b, n_layers, n_hidden_dec]
         
-        
-        ########################### 5. FINAL FC LAYER #########################
         fc_in = torch.cat((embedded_trg.squeeze(1),           #[b, n_embed]
                            weighted_sum.squeeze(1),           #[b, n_hidden_enc*2]
                            last_layer_dec.squeeze(1)), dim=1) #[b, n_hidden_dec]                           
                            
-       
         output = self.fc_final(fc_in) #[b, n_output]
         
         return output, last_h_dec, att_weights
 
-# class BahdanauDecoder(nn.Module):
-#     def __init__(self, hidden_size, output_size, n_layers=1, drop_prob=0.1):
-#         super(BahdanauDecoder, self).__init__()
-#         self.hidden_size = hidden_size
-#         self.output_size = output_size
-#         self.n_layers = n_layers
-#         self.drop_prob = drop_prob
-
-#         self.embedding = nn.Embedding(self.output_size, self.hidden_size)
-    
-#         self.fc_hidden = nn.Linear(self.hidden_size, self.hidden_size, bias=False)
-#         self.fc_encoder = nn.Linear(self.hidden_size * 2, self.hidden_size, bias=False)
-#         self.weight = nn.Parameter(torch.FloatTensor(1, hidden_size))
-#         self.attn_combine = nn.Linear(self.hidden_size * 2, self.hidden_size)
-#         self.dropout = nn.Dropout(self.drop_prob)
-#         self.lstm = nn.LSTM(self.hidden_size*2, self.hidden_size, batch_first=True)
-#         self.classifier = nn.Linear(self.hidden_size, self.output_size)
-
-#     def forward(self, inputs, hidden, encoder_outputs):
-#         encoder_outputs = encoder_outputs.squeeze()
-#         # Embed input words
-#         embedded = self.embedding(inputs.type(torch.LongTensor)).view(1, -1)
-#         embedded = self.dropout(embedded)
-    
-#         # Calculating Alignment Scores
-#         # print(hidden[0].shape)
-#         # print(encoder_outputs.shape)
-#         x = torch.tanh(self.fc_hidden(hidden[0])+self.fc_encoder(encoder_outputs))
-#         alignment_scores = x.bmm(self.weight.unsqueeze(2))  
-
-#         # Softmaxing alignment scores to get Attention weights
-#         attn_weights = F.softmax(alignment_scores.view(1,-1), dim=1)
-    
-#         # Multiplying the Attention weights with encoder outputs to get the context vector
-#         context_vector = torch.bmm(attn_weights.unsqueeze(0),
-#                                 encoder_outputs.unsqueeze(0))
-    
-#         # Concatenating context vector with embedded input word
-#         output = torch.cat((embedded, context_vector[0]), 1).unsqueeze(0)
-#         # Passing the concatenated vector as input to the LSTM cell
-#         output, hidden = self.lstm(output, hidden)
-#         # Passing the LSTM output through a Linear layer acting as a classifier
-#         output = F.log_softmax(self.classifier(output[0]), dim=1)
-#         return output, hidden, attn_weights
-
-# class AttnDecoderRNN(nn.Module):
-#     def __init__(self, hidden_size, output_size, dropout_p=0.1):
-#         super(AttnDecoderRNN, self).__init__()
-#         self.hidden_size = hidden_size
-#         self.output_size = output_size
-#         self.dropout_p = dropout_p
-
-#         self.embedding = nn.Embedding(self.output_size, self.hidden_size)
-#         self.attn = nn.Linear(self.hidden_size * 2, self.max_length)
-#         self.attn_combine = nn.Linear(self.hidden_size * 2, self.hidden_size)
-#         self.dropout = nn.Dropout(self.dropout_p)
-#         self.gru = nn.GRU(self.hidden_size, self.hidden_size)
-#         self.out = nn.Linear(self.hidden_size, self.output_size)
-
-#     def forward(self, input, hidden, encoder_outputs):
-#         embedded = self.embedding(input).view(1, 1, -1)
-#         embedded = self.dropout(embedded)
-
-#         attn_weights = F.softmax(
-#             self.attn(torch.cat((embedded[0], hidden[0]), 1)), dim=1)
-#         attn_applied = torch.bmm(attn_weights.unsqueeze(0),
-#                                  encoder_outputs.unsqueeze(0))
-
-#         output = torch.cat((embedded[0], attn_applied[0]), 1)
-#         output = self.attn_combine(output).unsqueeze(0)
-
-#         output = F.relu(output)
-#         output, hidden = self.gru(output, hidden)
-
-#         output = F.log_softmax(self.out(output[0]), dim=1)
-#         return output, hidden, attn_weights
-
-#     def initHidden(self):
-#         return torch.zeros(1, 1, self.hidden_size, device=device)
-
 class lstm_att(nn.Module):
-    """Convoluted sequence to sequence for AIS trajectories."""
-
     def __init__(self, config, partition_model = None):
         super().__init__()
 
@@ -437,7 +289,6 @@ class lstm_att(nn.Module):
             inputs_real = x
             inputs = idxs
             targets = None
-            
         batchsize, seqlen, _ = inputs.size()
         assert seqlen <= self.max_seqlen, "Cannot forward, model block size is exhausted."
 
@@ -455,16 +306,17 @@ class lstm_att(nn.Module):
             dec_token_embeddings = torch.cat((dec_lat_embeddings, dec_lon_embeddings, dec_sog_embeddings, dec_cog_embeddings),dim=-1)
         position_embeddings = self.pos_emb[:, :seqlen, :] # each position maps to a (learnable) vector (1, seqlen, n_embd)
         fea = self.drop(token_embeddings + position_embeddings)
-        self.encoder = EncoderRNN(32, 768)
-        self.decoder = Decoder(768,768,768)
-        encoder_hidden = self.encoder.initHidden()
+        self.encoder = EncoderRNN(768).cuda()
+        encoder_hidden = self.encoder.initHidden(fea.size(dim=0))
         encoder_output, encoder_hidden = self.encoder(fea, encoder_hidden)
+        n_embed = 768
         if not with_targets:
-            dec_targets=self.z
+            dec_targets=encoder_output
+            n_embed = n_embed * 2
         else:
             dec_targets = self.drop(dec_token_embeddings + position_embeddings)
-        ###########################  2. DECODER  ##############################
-        decoder_hidden = encoder_hidden      #[b, n_layers, n_hidden_dec]
+        self.decoder = Decoder(n_embed,768,768).cuda()
+        decoder_hidden = encoder_hidden
         
         trg_seq_len = dec_targets.size(1)
         
@@ -472,25 +324,15 @@ class lstm_att(nn.Module):
         output = dec_targets[:, 0, :]
         n_output = 768
         
-        outputs = torch.zeros(b, trg_seq_len, n_output)
+        outputs = torch.zeros(b, trg_seq_len, n_output).cuda()
         for t in range(1, trg_seq_len, 1):
             output, decoder_hidden, attn_weights = self.decoder(output, decoder_hidden, encoder_output)
-            outputs[:, t, :] = output #output: [b, n_output]
-
-            # if random.random() < 0.5:
+            outputs[:, t, :] = output
             output = dec_targets[:, t]
-            # else:
-            #     output = output.max(dim=2)[1]
-            #     print("no shesh")
-            #     print(output.shape)
-        # attn_weights = self.attention(decoder_hidden[0], encoder_output)
-        # decoder_output, decoder_hidden, decoder_attention = self.decoder(dec_targets, decoder_hidden[0], encoder_output, attn_weights)
         self.p=position_embeddings
         self.e=token_embeddings
         fea=fea+outputs
-        #fea2=fea.reshape(fea.shape[0], fea.shape[2], fea.shape[1])
-        #fea2 = self.ln_f(fea2) # (bs, seqlen, n_embd)
-        logits = self.head(fea) # (bs, seqlen, full_size) or (bs, seqlen, n_embd)
+        logits = self.head(fea)
         
         lat_logits, lon_logits, sog_logits, cog_logits =\
             torch.split(logits, (self.lat_size, self.lon_size, self.sog_size, self.cog_size), dim=-1)
